5d0accc8392d094d41ed6ec0ff479d1f334ee7c6
sayandeeydey06 <sayandeepdey2919@gmail.com>
2026-01-09 11:56:27 +0530

    Initial commit: AI Financial Document Intelligence

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     setResult(null);
 
     try {
-      const res = await fetch("https://ai-financial-backend-xm2p.onrender.com/analyze", {
+      const res = await fetch("https://ai-financial-document-intelligence-1.onrender.com/analyze", {
         method: "POST",
         body: formData,
       });
